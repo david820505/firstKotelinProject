de61a95f6dc000dff3079abe84d5399091725863
DavidHuang <d65044713@gmail.com>
2020-08-24 23:26:10 +0800

    Variable

--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -5,6 +5,14 @@ fun main(args: Array<String>) {
 //    Human().Hello()
     val h = Human()
     h.Hello()
+    //Int
+    //Float
+    //val(unchangable) or var(changable)
+    val age = 19
+    val age1 : Int = 19
+    var weight = 66.5f
+    val name: String
+    name = "Hank"
 }
 
 //在同個Package會跟JAVA衝突
